3d8ca0bb7a6bc9d829a6c6b66caa03ed4b200758
Mark Walker <markrwalker@gmail.com>
2013-05-20 13:16:56 -0400

    Removed db password from repo

--- mobile/checklogin.php
+++ mobile/checklogin.php
@@ -1,14 +1,6 @@
 <?php
 
-$host="localhost"; // Host name
-$username="higgy"; // Mysql username
-$password="vfCXV2zZjHzeFb5L"; // Mysql password
-$db_name="higgy_app"; // Database name
-$tbl_name="users"; // Table name
-
-// Connect to server and select databse.
-mysql_connect("$host", "$username", "$password")or die("cannot connect");
-mysql_select_db("$db_name")or die("cannot select DB");
+include('config.php');
 
 // username and password sent from form
 $password = $_POST['password'];
@@ -19,7 +11,7 @@ $header = 'Location: index.php';
 $password = stripslashes($password);
 $password = mysql_real_escape_string($password);
 
-$sql="SELECT * FROM $tbl_name WHERE password='$password'";
+$sql="SELECT * FROM users WHERE password='$password'";
 $result=mysql_query($sql);
 
 // Mysql_num_row is counting table row
@@ -29,12 +21,12 @@ if ($count==1) {
 	session_register("higgy_user");
 	header($header);
 } else {
-include('includes/header.php'); ?>
+	include('includes/header.php'); ?>
 		<div data-role="content">
 			<?php echo $sql; ?>
 			<p>Wrong PIN. Please contact the deck manager for the correct PIN.</p>
 		</div><!-- /content -->
 <?php 
-include('includes/footer.php');
+	include('includes/footer.php');
 }
 ?>
