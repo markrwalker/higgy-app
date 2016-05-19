39808015bbf457813b288cd9950f2c4b98d40c3e
Mark Walker <mark.r.walker@gmail.com>
2016-05-18 22:08:56 -0400

    Removed mobile from repo

--- mobile/includes/footer.php
+++
@@ -1,8 +0,0 @@
-
-		<div data-role="footer">
-			<h4>Higgyball 2015 Scoresheet App</h4>
-		</div><!-- /footer -->
-	</div><!-- /page -->
-
-</body>
-</html>
--- mobile/logout.php
+++
@@ -1,3 +0,0 @@
-<?php
-	setcookie("higgy_password",$password,time()-3600*24*3,"/");
-	header('Location: index.php');
